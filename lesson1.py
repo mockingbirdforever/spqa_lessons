7c697b82b7588fc3ab7ca913ce40b001cf12addb
mockingbirdforever <mockingbird.82@yandex.ru>
2025-01-06 16:18:50 +0300

    lesson 1

---
+++ lesson1.py
@@ -0,0 +1,25 @@
+
+
+my_heigh = 188
+my_name = 'Yura'
+my_name = 'Yura Laz'
+# pet_name = input('Как зовут вашего питомца?')
+
+def print_python():
+    print('Учу Python!')
+
+def print_letter(let):
+    print(let, end='')
+
+
+print(my_heigh)
+print(my_name)
+# print(f'Ваш любимчик — {pet_name}')
+print_python()
+print_letter('C')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')
+print_letter('т')
---
+++ lesson_1_task_5.py
@@ -0,0 +1,16 @@
+
+def silly_boy(num):
+    print(num, end='')
+
+
+silly_boy(8)
+silly_boy(8)
+silly_boy(0)
+silly_boy(0)
+silly_boy(5)
+silly_boy(5)
+silly_boy(5)
+silly_boy(3)
+silly_boy(5)
+silly_boy(3)
+silly_boy(5)
