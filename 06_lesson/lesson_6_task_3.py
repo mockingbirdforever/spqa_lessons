fa5ba6236a6ed013874d6b074f46e15c16b757fb
mockingbirdforever <mockingbird.82@yandex.ru>
2025-01-20 00:52:21 +0300

    lesson6&3

--- 06_lesson/lesson_6_task_3.py
+++ 06_lesson/lesson_6_task_3.py
@@ -6,14 +6,15 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 waiter = WebDriverWait(driver, 20)
 
-driver.get('http://uitestingplayground.com/ajax')
+driver.get('https://bonigarcia.dev/selenium-webdriver-java/loading-images.html')
 
 
 pic = waiter.until(
     EC.visibility_of_element_located((By.CSS_SELECTOR, '#landscape'))
 )
 
-text = driver.find_element(By.CSS_SELECTOR, '#award')
+text = driver.find_element(By.CSS_SELECTOR, 'img#award').get_attribute('src')
+print(text)
 
 driver.quit()
 
