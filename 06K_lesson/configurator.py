b34d0c74a8f986ff32c61aa62a8ceb3df6866d98
mockingbirdforever <mockingbird.82@yandex.ru>
2025-01-29 00:08:23 +0300

    lesson 6K fixes

---
+++ 06K_lesson/configurator.py
@@ -0,0 +1,24 @@
+# справочник тестовных данных
+
+# data for form test
+text_first_name = 'Иван'
+text_last_name = 'Петров'
+text_address = 'Ленина, 55-3'
+text_zip_code = ''
+text_city = 'Москва'
+text_country = 'Россия'
+text_email = 'test@skypro.com'
+text_phone = '+7985899998787'
+text_job_position = 'QA'
+text_company = 'SkyPro'
+text_n_a = 'N/A'
+
+
+# data for shop test
+username = 'standard_user'
+password = 'secret_sauce'
+test_sum = '$58.29'
+
+customer_first_name = 'Yurii'
+customer_last_name = 'Laz'
+customer_postal_code = '1234567'
--- 06K_lesson/test_03_shop.py
+++ 06K_lesson/test_03_shop.py
@@ -4,11 +4,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from time import sleep
+from configurator import *
 
-
-username = 'standard_user'
-password = 'secret_sauce'
-test_sum = '$58.29'
 
 driver = webdriver.Chrome()
 waiter = WebDriverWait(driver, 5)
@@ -16,13 +13,9 @@ waiter = WebDriverWait(driver, 5)
 
 driver.get('https://www.saucedemo.com/')
 
-username_input = driver.find_element(By.CSS_SELECTOR, '#user-name')
-password_input = driver.find_element(By.CSS_SELECTOR, '#password')
-login_button = driver.find_element(By.CSS_SELECTOR, '#login-button')
-
-username_input.send_keys(username)
-password_input.send_keys(password)
-login_button.click()
+driver.find_element(By.CSS_SELECTOR, '#user-name').send_keys(username)
+driver.find_element(By.CSS_SELECTOR, '#password').send_keys(password)
+driver.find_element(By.CSS_SELECTOR, '#login-button').click()
 
 
 item_1 = driver.find_element(By.CSS_SELECTOR, '#add-to-cart-sauce-labs-backpack')
@@ -36,20 +29,13 @@ item_3.click()
 shopping_cart_button.click()
 
 
-checkout_button = driver.find_element(By.CSS_SELECTOR, '#checkout')
+driver.find_element(By.CSS_SELECTOR, '#checkout').click()
 
-checkout_button.click()
+driver.find_element(By.CSS_SELECTOR, '#first-name').send_keys(customer_first_name)
+driver.find_element(By.CSS_SELECTOR, '#last-name').send_keys(customer_last_name)
+driver.find_element(By.CSS_SELECTOR, '#postal-code').send_keys(customer_postal_code)
+driver.find_element(By.CSS_SELECTOR, '#continue').click()
 
-
-first_name_input = driver.find_element(By.CSS_SELECTOR, '#first-name')
-last_name_input = driver.find_element(By.CSS_SELECTOR, '#last-name')
-postal_code_input = driver.find_element(By.CSS_SELECTOR, '#postal-code')
-continue_button = driver.find_element(By.CSS_SELECTOR, '#continue')
-
-first_name_input.send_keys('Yurii')
-last_name_input.send_keys('Laz')
-postal_code_input.send_keys('1234567')
-continue_button.click()
 
 
 total_sum = driver.find_element(By.CSS_SELECTOR, '#checkout_summary_container > div > div.summary_info > div.summary_total_label').text
